4a6c4eb3b0f31f24f97354c2a4f9303e0b8e29ee
Dmitry Lapshin <dimaviolinist@gmail.com>
2022-08-17 02:51:08 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,33 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  
 
-    }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    this.lastAnswer = max; 
+    this.currentAnswer = Math.ceil(this.max / 2); 
+    this.diff = this.max - this.currentAnswer; 
+  }
 
-    guess() {
+  guess() {
+    //console.log(this.lastAnswer);
+    //console.log(this.currentAnswer);
+    console.log(this.diff);
+    this.lastAnswer = this.currentAnswer;
+    return this.currentAnswer;
+  }
 
-    }
+  lower() {
+    this.diff = Math.ceil(this.diff / 2);
+    this.currentAnswer = this.lastAnswer - this.diff;
+  }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.diff = Math.ceil(this.diff / 2);
+    this.currentAnswer = this.lastAnswer + this.diff;
+  }
 }
 
 module.exports = GuessingGame;
